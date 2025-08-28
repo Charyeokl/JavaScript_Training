d35cfd479f20c05912f9826d633b069e3693583c
CMTLaki <156497392+Charyeokl@users.noreply.github.com>
2025-08-28 15:30:19 -0500

    updated sandbox.js

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,1 +1,25 @@
-alert('Hello World!');
+/* Numbers data types
+let age = 25;
+let year = 2019;
+
+// log thiings to console
+// console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points); 
+
+var score = 75;
+console.log(score); */
+
+// Strings data types
+
+// string concatenation
+
+// getting characters
+
+// string length
+
+// string methods
