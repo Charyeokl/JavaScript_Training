101fe9242254cdf3a272f93a0fe85312f40c6d84
CMTLaki <156497392+Charyeokl@users.noreply.github.com>
2025-08-29 08:45:00 -0500

    string data types

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -14,12 +14,33 @@ console.log(points);
 var score = 75;
 console.log(score); */
 
+/*
 // Strings data types
+console.log('Hello, world');
+
+let email = 'mario@thenetninja.uk';
+console.log(email);
 
 // string concatenation
+let firstName = 'Cini';
+let lastName = 'Laki';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
 
 // getting characters
+console.log(fullName[2]);
 
 // string length
+console.log(fullName.length);
 
 // string methods
+console.log(fullName.toUpperCase());
+// storing a result in a variable
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index); */
+
+// Common string methods
