29d5e8ecf9cfbe606bc35b0e45c0724a96775e8f
CMTLaki <156497392+Charyeokl@users.noreply.github.com>
2025-08-29 09:55:39 -0500

    using template strings

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -92,3 +92,27 @@ let likes = 10;
 let result = 'the blog has ' + likes + ' likes';
 console.log(result);
 */
+
+/*
+// template strings
+const title = 'Best reads of 2019';
+const author = 'Mario';
+const likes = 30;
+
+// concatenation way
+// let result = 'The blog called ' + title + 'by' + author + ' has ' + likes + ' likes';
+// console.log(result);
+
+// template string way
+let result = `The blog called ${title} by ${author} has ${likes} likes`;
+console.log(result);
+
+// create html template
+let html = `
+    <h2>${title}</h2>
+    <p>By ${author}</p>
+    <span>This blog has ${likes} likes</span>
+`;
+console.log(html);
+
+*/
