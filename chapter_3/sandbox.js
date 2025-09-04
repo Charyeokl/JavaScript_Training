e78bf05345e270bba8d9a526053e6c9b8f9892af
CMTLaki <156497392+Charyeokl@users.noreply.github.com>
2025-09-04 13:58:33 -0500

    for loop example

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -4,3 +4,11 @@
 //}
 
 //console.log('loop finished');
+
+const names = ['shaun', 'mario', 'luigi'];
+
+for (let i = 0; i < names.length; i++){
+    // console.log(names[i]);
+    let html = `<div>${names[i]}</div>`;
+    console.log(html)
+}
